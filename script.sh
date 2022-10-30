c497c4aed02c94d5aa08d9ade847109857ef194d
Alexandre Yuji Kajihara <alexandre.ykz@gmail.com>
2022-10-30 16:28:28 -0300

    Up

--- script.sh
+++ script.sh
@@ -4,7 +4,11 @@ threshold=20
 taxon=specific_epithet
 metric=f1_weighted
 
-${py} main.py -i ../dataset_gimp/imagens_george/features/${colormode}/segmented_unet/512/patch\=3/${taxon}/20/mobilenetv2/horizontal -l ../dataset_gimp/imagens_george/imagens/${colormode}/${taxon}/512/20/label.txt -m ${metric}
+for res in 512; do
+	for e in mobilenetv2; do
+		${py} main.py -i ../dataset_gimp/imagens_george/features/${colormode}/segmented_unet/${res}/patch\=3/${taxon}/10/${e}/horizontal -l ../dataset_gimp/imagens_george/imagens/${colormode}/${taxon}/${res}/10/label.txt -m ${metric}
+	done
+done
 
 for res in 256 400 512; do
 	for e in mobilenetv2 resnet50v2 vgg16; do
