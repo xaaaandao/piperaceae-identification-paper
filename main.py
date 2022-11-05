9a37a6fc43c00701287c700c9ebd5643357d779d
Alexandre Yuji Kajihara <alexandre.ykz@gmail.com>
2022-11-05 18:59:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -33,13 +33,13 @@ def main(list_user_input, labels, metric):
 
     current_datetime = datetime.datetime.now().strftime('%d-%m-%Y-%H-%M-%S')
     list_data_input = []
-    list_data_input = ['../dataset_gimp/imagens_george/features/RGB/segmented_unet/256/patch=3/specific_epithet/5/vgg16/horizontal']
+    # list_data_input = ['../dataset_gimp/imagens_george/features/RGB/segmented_unet/256/patch=3/specific_epithet/5/vgg16/horizontal']
     if len(list_user_input) == 0 and len(list_data_input) == 0:
         raise ValueError(f'list data input is empty')
 
     list_data_input = list_data_input + [i for i in list(list_user_input) if i not in list_data_input]
     print(f'quantidade de entradas: {len(list_data_input)}, filname labels: {labels}')
-    labels = '../dataset_gimp/imagens_george/imagens/RGB/specific_epithet/256/5/label.txt'
+    # labels = '../dataset_gimp/imagens_george/imagens/RGB/specific_epithet/256/5/label.txt'
     list_labels = get_list_label(labels)
 
     if len(list_data_input) > 0 and len(list_labels) > 0:
