6c3ed3ef40791bc659fea7f6bce87a105d2b4bdf
Alexandre Yuji Kajihara <alexandre.ykz@gmail.com>
2022-11-05 17:21:58 -0300

    Update script

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -30,11 +30,13 @@ C
 save_mean.py,a/1/a1884564cb64e1960bfbe1993e2671c20948cbf0
 H
 extractor/get_features.m,8/c/8cf5fb3ecbbac0d0624546051018cdde55b31567
-A
-read_result.ipynb,d/e/de2c5fe303145e1e732f3c8c004c5bd6937702f1
 @
 requirements.txt,1/9/19359a61ae2446b51b549167b014da2fcf265768
 =
 save_model.py,2/f/2f6b76eb354202792149b3c90f3c406239ebf257
 <
 handcraft.py,3/0/306cac00c27f870d7189390542e0e1ee2cf275c7
+7
+save.py,a/4/a4ab0ebcf0579eb7878e06e3c2c6a430fc5d8542
+=
+save_top_k.py,f/5/f5fe18d5be64863b39513ad401a655141c4f212e
--- script.sh
+++ script.sh
@@ -1,12 +1,14 @@
+#!/bin/bash
+
 py=~/miniconda3/bin/python
 colormode=RGB
 taxon=specific_epithet
-th=20
+threshold=20
 metric=f1_weighted
 
-for res in 512; do
+for res in 256 400 512; do
 	for e in mobilenetv2 resnet50v2 vgg16; do
-		${py} main.py -i ../dataset_gimp/imagens_george/features/${colormode}/segmented_unet/${res}/patch\=3/${taxon}/${th}/${e}/horizontal -l ../dataset_gimp/imagens_george/imagens/${colormode}/${taxon}/${res}/${th}/label.txt -m ${metric}
+		echo ${e} ${res} ${threshold} ${colormode} ${metric}
+		${py} main.py -i ../dataset_gimp/imagens_george/features/${colormode}/segmented_unet/${res}/patch\=3/${taxon}/${threshold}/${e}/horizontal -l ../dataset_gimp/imagens_george/imagens/${colormode}/${taxon}/${res}/${threshold}/label.txt -m ${metric}
 	done
 done
-
