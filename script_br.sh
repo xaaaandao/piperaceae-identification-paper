e601548363b353cafb3cc6c9361869d125ca0704
Alexandre Yuji Kajihara <alexandre.ykz@gmail.com>
2022-12-11 22:14:09 -0300

    Update script_br.sh

--- script_br.sh
+++ script_br.sh
@@ -2,16 +2,20 @@
 py=~/miniconda3/bin/python
 taxon=specific_epithet
 metric=f1_weighted
-for img_size in 512
+
+for img_size in 256 400 512;
 do
-	for extractor in vgg16
+	for extractor in vgg16;
 	do
-		for color in RGB
+		for color in RGB;
 		do
-			for threshold in 20
+			for threshold in 20;
 			do
-				echo ${extractor} ${img_size} ${threshold} ${color} ${metric}
-				${py} main.py -i ../dataset_gimp/imagens_br/features/${color}/segmented_unet/${img_size}/patch\=3/${taxon}/${threshold}/${extractor}/horizontal -l ../dataset_gimp/imagens_br/imagens/${color}/${taxon}/${img_size}/${threshold}/label2.txt -m ${metric}
+			    for regiao in "Sul";
+			    do
+                    echo ${extractor} ${img_size} ${threshold} ${color} ${metric}
+                    ${py} main.py -i ../dataset_gimp/imagens_regioes/features/${color}/segmented_unet/${img_size}/patch\=3/${regiao}/${taxon}/${threshold}/${extractor}/horizontal -l ../dataset_gimp/imagens_regioes/imagens/${color}/${img_size}/${regiao}/${threshold}/label2.txt -m ${metric}
+                done
 			done
 		done
 	done
