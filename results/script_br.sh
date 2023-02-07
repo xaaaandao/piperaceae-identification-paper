ee42452e815b4791ceb1fc7ecc0c3084cc836f5c
Alexandre Kajihara <alexandre.ykz@gmail.com>
2023-02-07 16:11:50 -0300

    update script results

--- results/script_br.sh
+++ results/script_br.sh
@@ -5,6 +5,6 @@ py=~/miniconda3/bin/python
 for threshold in 5 10 20; do
 	for color in rgb grayscale; do
 		mkdir -p /media/xandao/c2f58d30-ff2c-47f7-95af-91ad6fd69760/resultados/br/${color}/sheets_${threshold}
-		$py read_result.py -i /media/xandao/c2f58d30-ff2c-47f7-95af-91ad6fd69760/resultados/br/novos --threshold ${threshold} -o /media/xandao/c2f58d30-ff2c-47f7-95af-91ad6fd69760/resultados/br/${color}/sheets_${threshold} -c ${color} --taxon specific_epithet
+		$py read_result.py -i /media/xandao/c2f58d30-ff2c-47f7-95af-91ad6fd69760/resultados/br/novos --threshold ${threshold} -o /media/xandao/c2f58d30-ff2c-47f7-95af-91ad6fd69760/resultados/br/${color}/sheets_${threshold} -c ${color} --taxon specific_epithet --metric f1
 	done
 done
