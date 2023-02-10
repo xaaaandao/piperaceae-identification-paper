a43d2a99b3819fe19ea630705409c510d488ffad
Alexandre Kajihara <alexandre.ykz@gmail.com>
2023-02-10 11:16:41 -0300

    update svm

--- script_george.sh
+++ script_george.sh
@@ -4,11 +4,11 @@ METRIC=f1_weighted
 DIR_FEATURES=../dataset_gimp/imagens_george/features
 DIR_LABEL=../dataset_gimp/imagens_george/imagens
 
-for img_size in 256 400 512; do
-	for cnn in vgg16; do
-		for color in RGB; do
+for img_size in 400; do
+	for cnn in resnet50v2; do
+		for color in RGB grayscale; do
 			for patch in 3; do
-				for threshold in 5 10 20; do
+				for threshold in 5 10; do
 					echo ${cnn} ${img_size} ${threshold} ${color} ${patch} ${METRIC}
 					python main.py -i ${DIR_FEATURES}/${color}/segmented_unet/${img_size}/patch=${patch}/${TAXON}/${threshold}/${cnn}/horizontal -l ${DIR_LABEL}/${color}/${TAXON}/${img_size}/${threshold}/label2.txt -m ${METRIC} --pca
 				done
