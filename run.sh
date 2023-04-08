d9260de2fa7b593444dae642423b86c5f380997d
Alexandre Kajihara <alexandre.ykz@gmail.com>
2023-04-08 20:50:54 -0300

    update script to run

--- run.sh
+++ run.sh
@@ -4,14 +4,15 @@ METRIC=f1_weighted
 DIR_INPUT=/home/xandao/Imagens
 
 for dataset in pr_dataset_features; do
-  for image_size in 256 400 512; do
-    for cnn in vgg16; do
-      for color in GRAYSCALE RGB;	do
-        for threshold in 5 10 20; do
-          echo ${cnn} ${size} ${threshold} ${color} ${METRIC}
-          python main.py -i ${DIR_INPUT}/${color}/${image_size}/${TAXON}/${threshold}/${cnn} -c 'DecisionTreeClassifier'
+    for image_size in 256 400 512; do
+        for cnn in vgg16; do
+            for color in GRAYSCALE RGB;	do
+                for threshold in 5 10 20; do
+                    echo ${cnn} ${size} ${threshold} ${color} ${METRIC}
+                    python setup.py build_ext --inplace
+                    python main.py -i ${DIR_INPUT}/${dataset}/${color}/${image_size}/${TAXON}/${threshold}/${cnn} -c 'DecisionTreeClassifier'
+                done
+            done
         done
-      done
     done
-  done
 done
