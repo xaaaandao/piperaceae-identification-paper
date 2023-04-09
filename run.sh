7b7b8588c86b6d72cad99bdb1b8afdde24524865
Alexandre Kajihara <alexandre.ykz@gmail.com>
2023-04-09 17:55:46 -0300

    up up up

--- run.sh
+++ run.sh
@@ -8,8 +8,8 @@ for dataset in pr_dataset_features; do
         for cnn in vgg16; do
             for color in GRAYSCALE RGB;	do
                 for threshold in 5 10 20; do
-                    for classifier in DecisionTreeClassifier KNeighborsClassifier MLPClassifier RandomForestClassifier         SVC; do
-                        echo ${cnn} ${size} ${threshold} ${color} ${METRIC}
+                    for classifier in DecisionTreeClassifier KNeighborsClassifier RandomForestClassifier SVC; do
+                        echo ${cnn} ${size} ${threshold} ${color} ${METRIC} ${classifier}
                         python setup.py build_ext --inplace
                         # /home/xandao/Imagens/pr_dataset_features/RGB/256/specific_epithet_trusted/20/vgg16
                         python main.py -i ${DIR_INPUT}/${dataset}/${color}/${image_size}/${TAXON}/${threshold}/${cnn} -c ${classifier} -p
