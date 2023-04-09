8e35c1e63a1cf862c666a5e3d7e2909d41ab37a9
Alexandre Kajihara <alexandre.ykz@gmail.com>
2023-04-09 12:02:50 -0300

    ups

--- dataset.py
+++ dataset.py
@@ -30,7 +30,7 @@ def load_dataset_informations(input):
     logging.info('[INFO] n_samples: %s n_features: %s patch: %s' % (n_samples, n_features, patch))
 
     input_path = input_path.replace('_features', '')
-    input_path = input_path.replace('/media/kingston500/mestrado/dataset', '/home/xandao/Imagens')
+    # input_path = input_path.replace('/media/kingston500/mestrado/dataset', '/home/xandao/Imagens')
     if not os.path.exists(input_path):
         raise SystemExit('input path %s not exists' % input_path)
 
