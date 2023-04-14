a557de1a33c593246aec21bc83f9d77c542fdee3
Alexandre Kajihara <alexandre.ykz@gmail.com>
2023-04-14 16:53:43 -0300

    up run.sh

--- run.sh
+++ run.sh
@@ -4,15 +4,22 @@ METRIC=f1_weighted
 # DIR_INPUT=/home/xandao/Imagens
 DIR_INPUT=/media/kingston500/mestrado/dataset
 
-for dataset in pr_dataset_features; do
+for dataset in regions_dataset_features; do
     for image_size in 256 400 512; do
-        for cnn in surf64.txt; do
+        for cnn in vgg16; do
             for color in GRAYSCALE RGB;	do
                 for threshold in 5 10 20; do
                     for classifier in DecisionTreeClassifier KNeighborsClassifier RandomForestClassifier SVC; do
-                        echo ${cnn} ${size} ${threshold} ${color} ${METRIC} ${classifier}
                         python setup.py build_ext --inplace
-                        python main.py -i ${DIR_INPUT}/${dataset}/${color}/${image_size}/${TAXON}/${threshold}/${cnn} -c ${classifier} -p
+                        if [regions_dataset_features = dataset]; then
+                            for region in Norte Nordeste Centro-Oeste Sul Sudeste; do
+                                echo ${cnn} ${size} ${threshold} ${color} ${METRIC} ${classifier} ${region}
+                                python main.py -i ${DIR_INPUT}/${dataset}/${color}/${region}/${image_size}/${TAXON}/${threshold}/${cnn} -c ${classifier} -p
+                            done
+                        else
+                            echo ${cnn} ${size} ${threshold} ${color} ${METRIC} ${classifier}
+                            python main.py -i ${DIR_INPUT}/${dataset}/${color}/${image_size}/${TAXON}/${threshold}/${cnn} -c ${classifier} -p
+                        if
                     done
                 done
             done
