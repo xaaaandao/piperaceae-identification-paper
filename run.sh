158b231c4cb1b45a62a5c221900e6d51b9acfdeb
Alexandre Kajihara <alexandre.ykz@gmail.com>
2023-05-01 18:01:29 -0300

    up

--- run.sh
+++ run.sh
@@ -5,28 +5,31 @@ DIR_INPUT=/home/xandao/Imagens
 # DIR_INPUT=/media/kingston500/mestrado/dataset
 
 for dataset in pr_dataset_features; do
-    for image_size in 512; do
+    for image_size in 512 400 256; do
         for cnn in lbp.txt surf64.txt; do
             for color in GRAYSCALE; do
                 for threshold in 20 10 5; do
-                    INPUT=${DIR_INPUT}/${dataset}/${color}/${TAXON}/${image_size}
-#                    for classifier in DecisionTreeClassifier; do
-#                    for classifier in KNeighborsClassifierClassifier; do
-#                    for classifier in RandomForestClassifier; do
-#                    for classifier in MLPClassifier; do
-#                    for classifier in SVC; do
-                    for classifier in DecisionTreeClassifier KNeighborsClassifier RandomForestClassifier MLPClassifier SVC; do
-                        python setup.py build_ext --inplace
-                        if [ "regions_dataset_features" = "$dataset" ]; then
-                            for region in Norte Nordeste Centro-Oeste Sul Sudeste; do
-                                echo ${cnn} ${size} ${threshold} ${color} ${METRIC} ${classifier} ${region}
-                                python main.py -i ${INPUT}/${region}/${threshold}/${cnn} -c ${classifier}
-                            done
-                        else
-
-                            echo ${cnn} ${size} ${threshold} ${color} ${METRIC} ${classifier}
-                            python main.py -i ${INPUT}/${threshold}/${cnn} -c ${classifier}
-                        fi
+                    for contrast in 1.8 1.5 1.2; do
+                        INPUT=${DIR_INPUT}/${dataset}_CONTRAST_${contrast}/${color}/${TAXON}/${image_size}
+    #                    for classifier in DecisionTreeClassifier; do
+    #                    for classifier in KNeighborsClassifierClassifier; do
+    #                    for classifier in RandomForestClassifier; do
+    #                    for classifier in MLPClassifier; do
+    #                    for classifier in SVC; do
+                        for classifier in DecisionTreeClassifier KNeighborsClassifier RandomForestClassifier MLPClassifier SVC; do
+                            python setup.py build_ext --inplace
+                            if [ "regions_dataset_features" = "$dataset" ]; then
+                                for region in Norte Nordeste Centro-Oeste Sul Sudeste; do
+                                    echo ${cnn} ${size} ${threshold} ${color} ${METRIC} ${classifier} ${region}
+                                    echo ${INPUT}
+                                    python main.py -i ${INPUT}/${region}/${threshold}/${cnn} -c ${classifier}
+                                done
+                            else
+                                echo ${cnn} ${size} ${threshold} ${color} ${METRIC} ${classifier}
+                                echo ${INPUT}
+                                python main.py -i ${INPUT}/${threshold}/${cnn} -c ${classifier}
+                            fi
+                        done
                     done
                 done
             done
