c999bead3db79c5a287ba146096927c217e64bc6
Alexandre Kajihara <alexandre.ykz@gmail.com>
2023-05-03 13:18:58 -0300

    up

--- run.sh
+++ run.sh
@@ -4,13 +4,13 @@ METRIC=f1_weighted
 DIR_INPUT=/home/xandao/Imagens
 # DIR_INPUT=/media/kingston500/mestrado/dataset
 
-for dataset in pr_dataset_features; do
+for dataset in regions_dataset_features; do
     for image_size in 512 400 256; do
-        for cnn in lbp.txt surf64.txt; do
+        for cnn in vgg16; do
             for color in GRAYSCALE; do
                 for threshold in 20 10 5; do
-                    for contrast in 1.8 1.5 1.2; do
-                        INPUT=${DIR_INPUT}/${dataset}_CONTRAST_${contrast}/${color}/${TAXON}/${image_size}
+                    for contrast in 1.2; do
+                        INPUT=${DIR_INPUT}/${dataset}_CONTRAST_${contrast}/${color}/${TAXON}
     #                    for classifier in DecisionTreeClassifier; do
     #                    for classifier in KNeighborsClassifierClassifier; do
     #                    for classifier in RandomForestClassifier; do
@@ -21,13 +21,13 @@ for dataset in pr_dataset_features; do
                             if [ "regions_dataset_features" = "$dataset" ]; then
                                 for region in Norte Nordeste Centro-Oeste Sul Sudeste; do
                                     echo ${cnn} ${size} ${threshold} ${color} ${METRIC} ${classifier} ${region}
-                                    echo ${INPUT}
+                                    echo ${INPUT}/${region}/${image_size}/${threshold}/${cnn}
                                     python main.py -i ${INPUT}/${region}/${threshold}/${cnn} -c ${classifier}
                                 done
                             else
                                 echo ${cnn} ${size} ${threshold} ${color} ${METRIC} ${classifier}
                                 echo ${INPUT}
-                                python main.py -i ${INPUT}/${threshold}/${cnn} -c ${classifier}
+                                python main.py -i ${INPUT}/${image_size}/${threshold}/${cnn} -c ${classifier}
                             fi
                         done
                     done
