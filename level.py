2d3457a7efdbfaa151da251d0266d338e299eba1
xandao <alexandre.ykz@gmail.com>
2024-12-29 16:42:01 -0300

    up level.py

--- level.py
+++ level.py
@@ -6,11 +6,6 @@ class Level:
     specific_epithet: str = dataclasses.field(default=None)
     label: int = dataclasses.field(default=None)
 
-    def __init__(self, label, specific_epithet):
-        super().__init__()
-        self.label = label
-        self.specific_epithet = str(specific_epithet)
-
     def __eq__(self, label: int, specific_epithet: str) -> bool:
         """
         Verifica se o inteiro do label (f1, f2, f3, ...) é igual ao valor que está na classe
