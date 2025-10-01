7f63e07ed3da8c1ec5eeacd43049969ca93c92b2
xandao <alexandre.ykz@gmail.com>
2025-10-01 16:42:10 -0300

    up mult e sum rule

--- test_predict.py
+++ test_predict.py
@@ -26,21 +26,20 @@ class TestPredict(unittest.TestCase):
     def test_max_predict(self):
         self.y_test, self.y_score = max_rule(self.qtd_test, self.max_labels, self.patch, self.y_pred_proba)
         for yt, ys in zip(self.y_test, self.y_score):
+            print(yt, np.argmax(ys), ys)
             self.assertEqual(yt - 1, np.argmax(ys))
 
     def test_sum_predict(self):
         self.y_test, self.y_score = sum_rule(self.qtd_test, self.max_labels, self.patch, self.y_pred_proba)
-        for y in self.y_test:
-            for s in self.y_score:
-                print(s, np.max(s), s[y - 1], y)
-                self.assertEqual(s[y - 1], np.max(s))
+        for yt, ys in zip(self.y_test, self.y_score):
+            self.assertEqual(yt - 1, np.argmax(ys))
+
 
     def test_mult_predict(self):
         self.y_test, self.y_score = mult_rule(self.qtd_test, self.max_labels, self.patch, self.y_pred_proba)
-        for y in self.y_test:
-            for s in self.y_score:
-                print(s, np.max(s), s[y - 1], y)
-                self.assertEqual(s[y - 1], np.max(s))
+        for yt, ys in zip(self.y_test, self.y_score):
+            self.assertEqual(yt - 1, np.argmax(ys))
+
 
 if __name__ == '__main__':
     unittest.main()
